13b011c5129c582141a8b4ce40ca11a5f29b56bf
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-04-27 11:47:35 -0700

    I think I answered everything

---
+++ timea/client/scripts/models/book.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var app = app || {};
+
+const ENV = {};
+
+ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'insert cloud API server URL here';
+ENV.developmentApiUrl = 'insert local API server URL here';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+(function(module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function Book(rawBookObj) {
+    Object.keys(rawBookObj).forEach(key => this[key] = rawBookObj[key]);
+  }
+
+  Book.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+  }
+
+  Book.all = [];
+  Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+  Book.fetchAll = callback =>
+    $.get(`${ENV.apiUrl}/api/v1/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+
+  Book.fetchOne = (ctx, callback) =>
+    $.get(`${ENV.apiUrl}/api/v1/books/${ctx.params.book_id}`)
+      .then(results => ctx.book = results[0])
+      .then(callback)
+      .catch(errorCallback);
+
+  Book.create = book =>
+    $.post(`${ENV.apiUrl}/api/v1/books`, book)
+      .then(() => page('/'))
+      .catch(errorCallback);
+
+  Book.update = (book, bookId) =>
+      $.ajax({
+        url: `${ENV.apiUrl}/api/v1/books/${bookId}`,
+        method: 'PUT',
+        data: book,
+      })
+      .then(() => page(`/books/${bookId}`))
+      .catch(errorCallback)
+
+  Book.destroy = id =>
+    $.ajax({
+      url: `${ENV.apiUrl}/api/v1/books/${id}`,
+      method: 'DELETE',
+    })
+    .then(() => page('/'))
+    .catch(errorCallback)
+
+  // COMMENT: Where is this method invoked? What is passed in as the 'book' argument when invoked? What callback will be invoked after Book.loadAll is invoked?
+  //invoked in the initsearchformpage the book is an array of books coming from the result of the google api query
+  Book.find = (book, callback) =>
+    $.get(`${ENV.apiUrl}/api/v1/books/find`, book)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback)
+
+  // COMMENT: Where is this method invoked? How does it differ from the Book.find method, above?
+  //invoked in the initsearchresultpage the book ia book coming from the result of the google api query
+  Book.findOne = isbn =>
+    $.get(`${ENV.apiUrl}/api/v1/books/find/${isbn}`)
+    .then(Book.create)
+    .catch(errorCallback)
+
+  module.Book = Book;
+})(app)
---
+++ timea/client/scripts/views/book-view.js
@@ -0,0 +1,132 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  $('.icon-menu').on('click', function(event) {
+    $('.nav-menu').slideToggle(350);
+  })
+
+  function resetView() {
+    $('.container').hide();
+    $('.nav-menu').slideUp(350);
+  }
+
+  const bookView = {};
+
+  bookView.initIndexPage = function(ctx, next) {
+    resetView();
+    $('.book-view').show();
+    $('#book-list').empty();
+    module.Book.all.forEach(book => $('#book-list').append(book.toHtml()));
+    next()
+  }
+
+  bookView.initDetailPage = function(ctx, next) {
+    resetView();
+    $('.detail-view').show();
+    $('.book-detail').empty();
+    let template = Handlebars.compile($('#book-detail-template').text());
+    $('.book-detail').append(template(ctx.book));
+
+    $('#update-btn').on('click', function() {
+      page(`/books/${$(this).data('id')}/update`);
+    });
+
+    $('#delete-btn').on('click', function() {
+      module.Book.destroy($(this).data('id'));
+    });
+    next()
+  }
+
+  bookView.initCreateFormPage = function() {
+    resetView();
+    $('.create-view').show();
+    $('#create-form').on('submit', function(event) {
+      event.preventDefault();
+
+      let book = {
+        title: event.target.title.value,
+        author: event.target.author.value,
+        isbn: event.target.isbn.value,
+        image_url: event.target.image_url.value,
+        description: event.target.description.value,
+      };
+
+      module.Book.create(book);
+    })
+  }
+
+  bookView.initUpdateFormPage = function(ctx) {
+    resetView();
+    $('.update-view').show()
+    $('#update-form input[name="title"]').val(ctx.book.title);
+    $('#update-form input[name="author"]').val(ctx.book.author);
+    $('#update-form input[name="isbn"]').val(ctx.book.isbn);
+    $('#update-form input[name="image_url"]').val(ctx.book.image_url);
+    $('#update-form textarea[name="description"]').val(ctx.book.description);
+
+    $('#update-form').on('submit', function(event) {
+      event.preventDefault();
+
+      let book = {
+        book_id: ctx.book.book_id,
+        title: event.target.title.value,
+        author: event.target.author.value,
+        isbn: event.target.isbn.value,
+        image_url: event.target.image_url.value,
+        description: event.target.description.value,
+      };
+
+      module.Book.update(book, book.book_id);
+    })
+  };
+
+// COMMENT: What is the purpose of this method?
+//it initializes the search form page
+  bookView.initSearchFormPage = function() {
+    resetView();
+    $('.search-view').show();
+    $('#search-form').on('submit', function(event) {
+      // COMMENT: What default behavior is being prevented here?
+      //page refresh
+      event.preventDefault();
+
+      // COMMENT: What is the event.target, below? What will happen if the user does not provide the information needed for the title, author, or isbn properties?
+      //event target is the form, if they don't fill out a field it is going to be blank (empty string)
+      let book = {
+        title: event.target.title.value || '',
+        author: event.target.author.value || '',
+        isbn: event.target.isbn.value || '',
+      };
+
+      module.Book.find(book, bookView.initSearchResultsPage);
+
+      // COMMENT: Why are these values set to an empty string?
+      // to clear out the user input 
+      event.target.title.value = '';
+      event.target.author.value = '';
+      event.target.isbn.value = '';
+    })
+  }
+
+  // COMMENT: What is the purpose of this method?
+  //it initiates the result page
+  bookView.initSearchResultsPage = function() {
+    resetView();
+    $('.search-results').show();
+    $('#search-list').empty();
+
+    // COMMENT: Explain how the .forEach() method is being used below.
+    //it goes through the books to adds the one searched to the book list, there below on the weird parent line 
+    module.Book.all.forEach(book => $('#search-list').append(book.toHtml()));
+    $('.detail-button a').text('Add to list').attr('href', '/');
+    $('.detail-button').on('click', function(e) {
+      // COMMENT: Explain the following line of code.
+      //this points to the book items template, where the third parent has the data-bookid attribute, where the book searched is to be appended
+      module.Book.findOne($(this).parent().parent().parent().data('bookid'))
+    });
+  }
+
+  module.bookView = bookView;
+})(app)
+
